78ce8e7773decb6b82334a2b5975c985bf97369a
joedder <camachojoedder@gmail.com>
2025-08-24 11:58:55 -0400

    creacion_models_user

--- Coffe_and_Tea/settings.py
+++ Coffe_and_Tea/settings.py
@@ -132,3 +132,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Importancion de modelo personalizado
+AUTH_USER_MODEL ='core.User'
+
+#Configuracion de archivos multimedia
+MEDIA_URL = '/media/'
+
+# Directorio donde se almacenarán los archivos multimedia
+MEDIA_ROOT = BASE_DIR / 'media'
--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    username=models.CharField(max_length=150, unique=True)
+    email=models.EmailField(unique=True)
+    phone_number=models.CharField(max_length=15, blank=True, null=True)
+    address=models.TextField(max_length=255, blank=True, null=True)
+    city=models.CharField(max_length=100, blank=True, null=True)
+    country=models.CharField(max_length=100, blank=True, null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    # REQUIRED_FIELDS=['email', 'phone_number'] #crecion de campos requeridos al crear el super usuario
+
+    class Meta:
+        db_table="users"
+        verbose_name='Usuario'
+        verbose_name_plural='Usuarios'
+    
+    def __str__(self):
+        return f"{self.username} ({self.email})"
