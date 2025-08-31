92001e10cedfc404eec0139d63712b489abe483a
joedder <camachojoedder@gmail.com>
2025-08-31 11:43:15 -0400

    creacion de las vistas de los comentarios

--- core/urls.py
+++ core/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('products/', views.products, name='products'),
+    # path('products/', views.products, name='products'),
     path('detail_product/', views.detail_product, name='detail_product'),
     path('login_view/', views.LoginView.as_view(), name='login_view'),
     path('register_view/', views.RegisterView.as_view(), name='register_view'),
+    path('product_view/', views.ProductView.as_view(), name='product_view'),
+    path('product_detail_view/<int:id>/', views.ProductDetailView.as_view(), name='product_detail_view'),
+    path('comment_detail_view/<int:id>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from core.models import *
 
 # Create your views here.
 
@@ -20,22 +21,66 @@ def detail_product(request):
 
 
 class LoginView(View):
+    template_name= 'pagina/Login.html'
     def get(self, request):
-        return render(request, 'pagina\Login.html')
+        return render(request, self.template_name)
     
     def post(self, request):
         pass
 
 class RegisterView(View):
+    template_name= 'pagina/Register.html'
     def get(self, request):
-        return render(request, 'pagina\Register.html')
+        return render(request, self.template_name)
 
     def post(self, request):
         pass
 
 class ProductView(View):
+    template_name = 'pagina/Producto.html'
     def get(self, request):
-        return render(request, 'pagina\Producto.html')
+        return render(request, self.template_name)
 
-    def post(self, request):
-        pass
+
+class ProductDetailView(View):
+    template_name = 'pagina/detail_product.html'
+    def get (self, request, id, *args, **kwargs):
+        product=get_object_or_404(Product, id=id)
+        comments=Comment.objects.filter(product_id=id)
+        return render(request, self.template_name, {'product':product, 'comments':comments})
+    
+    def post (self, request, id, *args, **kwargs):
+        body= request.POST.get("body")
+        product=Product.objects.filter(id=id).first() if id else None
+        current_user= request.user
+        print(body)
+        new_comment= Comment.objects.create(
+            body=body,
+            product=product,
+            user=current_user
+        )
+
+
+        return redirect('product_detail_view', id)
+class CommentDetailView(View):
+    template_name = 'pagina/detail_comment.html'
+    def get (self, request, id, *args, **kwargs):
+        comment=get_object_or_404(Comment, id=id)
+        comments=Comment.objects.filter(comment_id=id)
+        return render(request, self.template_name, {'comment':comment, 'comments':comments})
+    
+    def post (self, request, id, *args, **kwargs):
+        body= request.POST.get("body")
+        comment=Comment.objects.filter(id=id).first() if id else None
+        product = comment.product if comment else None
+        current_user= request.user
+        print(body)
+        new_comment= Comment.objects.create(
+            body=body,
+            comment=comment,
+            product=product,
+            user=current_user
+        )
+
+
+        return redirect('comment_detail_view', id)
